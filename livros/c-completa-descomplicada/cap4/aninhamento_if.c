7fa075527003f6429a7e0ec81a85a036751b6df5
Rafael Mota Moraes <rafaelmota622@gmail.com>
2024-05-11 15:23:08 -0300

    aninhamento de ifs

---
+++ livros/c-completa-descomplicada/cap4/aninhamento_if.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int num;
+    printf("Digite um numero: ");
+    scanf("%d", &num);
+
+    if (num == 10)
+    {
+        printf("O numero eh igual a 10\n");
+    }
+    else
+    {
+        if (num > 10)
+            printf("O numero eh maior que 10\n");
+        else
+            printf("O numero eh menor que 10\n");
+    }
+
+    return 0;
+}
