9322f8c951166a65610446e31e1305062e3cb801
Rafael Mota Moraes <rafaelmota622@gmail.com>
2024-05-20 18:24:26 -0300

    break no loop

---
+++ livros/c-completa-descomplicada/cap5/break.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, b;
+    printf("Digite o valor de A: ");
+    scanf("%d", &a);
+
+    printf("Digite o valor de B: ");
+    scanf("%d", &b);
+
+    while (a <= b)
+    {
+        a = a + 1;
+
+        if (a == 5)
+        {
+            break;
+        }
+
+        printf("%d \n", a);
+    }
+
+    return 0;
+}
