fddea65e52416e04f1101e98235ccb9b46048aaf
Rafael Mota Moraes <rafaelmota622@gmail.com>
2024-05-20 17:51:12 -0300

    for sem incremento

---
+++ livros/c-completa-descomplicada/cap5/for_sem_incremento.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    int a, b, c;
+
+    printf("Digite o valor de A: ");
+    scanf("%d", &a);
+    printf("Digite o valor de B: ");
+    scanf("%d", &b);
+
+    for (c = a; c <= b;)
+    {
+        printf("%d ", a);
+        c++;
+    }
+
+    return 0;
+}
