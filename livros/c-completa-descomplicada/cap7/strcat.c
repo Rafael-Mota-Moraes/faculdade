ca937b8de78faa0209a3e95992cd7c0cbffae2b9
Rafael Mota Moraes <rafaelmota622@gmail.com>
2024-05-24 19:15:06 -0300

    função strcat

---
+++ livros/c-completa-descomplicada/cap7/strcat.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main()
+{
+    char str1[15] = "Bom";
+    char str2[15] = " dia.";
+
+    strcat(str1, str2);
+    printf("%s\n", str1);
+    return 0;
+}
