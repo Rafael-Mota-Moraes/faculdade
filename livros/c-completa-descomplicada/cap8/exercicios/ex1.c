6342c313bb3d263d6bf364a4b43709baa5c6bad6
Rafael Mota Moraes <rafaelmota622@gmail.com>
2024-05-27 16:47:30 -0300

    ex1 arquivo

---
+++ livros/c-completa-descomplicada/cap8/exercicios/ex1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+Implemente um programa que leia o nome, a idade e o endereço de uma
+pessoa e armazene esses dados em uma estrutura. Em seguida, imprima na
+tela os dados da estrutura lida.
+*/
+
+int main()
+{
+    return 0;
+}
