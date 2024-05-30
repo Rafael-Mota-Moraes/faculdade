d682535348559976ca4e96510723d363558a4962
Rafael Mota Moraes <rafaelmota622@gmail.com>
2024-05-30 19:25:21 -0300

    estrutura passada por referência

---
+++ livros/c-completa-descomplicada/cap9/estrutura_por_referencia.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct ponto
+{
+    int x, y;
+};
+
+void atribui(struct ponto *p)
+{
+    (*p).x = 10;
+    (*p).y = 20;
+}
+
+int main()
+{
+    struct ponto p1;
+    atribui(&p1);
+    printf("x = %d\n", p1.x);
+    printf("x = %d\n", p1.y);
+
+    return 0;
+}
