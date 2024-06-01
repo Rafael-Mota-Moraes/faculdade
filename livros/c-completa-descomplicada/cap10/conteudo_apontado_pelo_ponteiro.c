e2a9533eda9cea601462ec557923d9c2479c7a15
Rafael Mota Moraes <rafaelmota622@gmail.com>
2024-06-01 15:43:00 -0300

    Mechendo no conteúdo apontado pelo ponteiro

---
+++ livros/c-completa-descomplicada/cap10/conteudo_apontado_pelo_ponteiro.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int *p, x = 10;
+    p = &x;
+    printf("Conteudo apontado por p: %d \n", *p);
+
+    *p = ++(*p);
+    printf("Conteudo apontado por p: %d \n", *p);
+
+    *p = (*p) * 10;
+    printf("Conteudo apontado por p: %d \n", *p);
+
+    return 0;
+}
