2205b5332f29db7c898a2da4a8d5d511b804a0b7
Rafael Mota Moraes <rafaelmota622@gmail.com>
2024-06-01 12:18:53 -0300

    Declaração de ponteiro

---
+++ livros/c-completa-descomplicada/cap10/declaracao_ponteiros.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    // Declara um ponteiro para int
+    int *p;
+
+    // Delcara um ponteiro para float
+    float *y;
+
+    // Declara um ponteiro para char
+    char *y;
+
+    // Declara uma variável do tipo int e um ponteiro para int
+    int soma, *p2;
+
+    return 0;
+}
