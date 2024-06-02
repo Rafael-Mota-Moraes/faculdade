581a4e3ef46b211f9925c4bb9cad67e51d4f0883
Rafael Mota Moraes <rafaelmota622@gmail.com>
2024-06-02 14:46:22 -0300

    Função free

---
+++ livros/c-completa-descomplicada/cap11/free.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int *p, i;
+
+    p = (int *)malloc(50 * sizeof(int));
+
+    if (p == NULL)
+    {
+        printf("Erro: Memória insuficiente!\n");
+        exit(1);
+    }
+
+    for (i = 0; i < 50; i++)
+        p[i] = i + 1;
+
+    for (i = 0; i < 50; i++)
+        printf("%d\n", p[i]);
+
+    free(p);
+    return 0;
+}
