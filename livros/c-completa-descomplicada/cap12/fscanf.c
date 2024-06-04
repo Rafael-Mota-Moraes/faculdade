1ef04ac3730f20d92c64f0b8fb45a61bf4d9d9bb
Rafael Mota Moraes <rafaelmota622@gmail.com>
2024-06-04 16:06:22 -0300

    fscanf

---
+++ livros/c-completa-descomplicada/cap12/fscanf.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    FILE *arq;
+    char texto[20], nome[20];
+    int i;
+    float a;
+    int result;
+
+    arq = fopen("ArqGrav.txt", "r");
+
+    if (arq == NULL)
+    {
+        printf("problemas na abertura do arquivo.\n");
+        exit(1);
+    }
+
+    fscanf(arq, "%s%s", texto, nome);
+    printf("%s %s\n", texto, nome);
+
+    fscanf(arq, "%s %d", texto, &i);
+    printf("%s %d\n", texto, i);
+
+    fscanf(arq, "%s %f", texto, &a);
+    printf("%s %f\n", texto, a);
+
+    return 0;
+}
